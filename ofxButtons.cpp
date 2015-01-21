8b6c6ca61e7d926af60bb18c2387165494b6d239
Yang Fei <249893977@qq.com>
2015-01-21 15:55:39 +0800

    ofxButtons

---
+++ ofxButtons.cpp
@@ -0,0 +1,145 @@
+#include "ofxButtons.h"
+namespace cn_seefeel{
+//--------------------------------class ofxButtons -----------------------
+ofxButtons::ofxButtons(void)
+{
+	//void addListener();
+
+	_bCentered = false;
+}
+
+
+ofxButtons::~ofxButtons(void)
+{
+	//void removeListener();
+}
+
+void ofxButtons::setup(const string & name){
+	ofLogNotice() << "ofxButton::Setup of " + name; 
+
+	gui.addPage(name).setXMLName(name + ".xml");
+	gui.addTitle("Buttons");
+
+	map<int, button>::iterator it = _buttons.begin();
+	map<int, button>::iterator itend = _buttons.end();
+
+	for ( ; it != itend; ++it){
+		gui.addSlider(ofToString(it->first) + " X ", it->second._pos.x, 0, ofGetWidth());
+		gui.addSlider(ofToString(it->first) + " Y ", it->second._pos.y, 0, ofGetHeight());
+	}
+
+	gui.loadFromXML();
+	gui.setDefaultKeys(true);
+}
+
+void ofxButtons::setPosition(const int& btnName, const ofPoint &pos){
+
+	map<int, button>::iterator it = _buttons.find(btnName);
+	if (it != _buttons.end()){
+		if (ofDist(it->second._pos.x, it->second._pos.y, pos.x, pos.y)>20){
+			it->second._pos = pos;
+		}
+	}else{
+		ofLogVerbose() << "Not found button : " + btnName;
+	}
+}
+
+void ofxButtons::SetButton(const int &name, ofImage *b, int id){
+	ofLogNotice() << "ofxButton::Set Button : " + ofToString(name);
+	_buttons[name]._img[id] = b;
+}
+
+void ofxButtons::draw(){
+
+	map<int, button>::iterator it = _buttons.begin();
+	map<int, button>::iterator itend = _buttons.end();
+
+	for (; it != itend; ++it){
+		// draw every enabled buttons
+		if(it->second._enabled){
+			it->second._img[it->second._id]->draw(it->second._pos.x, it->second._pos.y);
+		}
+	}
+}
+
+void ofxButtons::draw(int btnId){
+	//ofEnableAlphaBlending();
+	map<int, button>::iterator it = _buttons.find(btnId);
+	ofSetRectMode(OF_RECTMODE_CENTER);
+	if (it != _buttons.end()){
+		it->second._img[it->second._id]->draw(it->second._pos.x, it->second._pos.y);
+	}
+	ofSetRectMode(OF_RECTMODE_CORNER);
+	//ofDisableAlphaBlending();
+}
+
+ofRectangle ofxButtons::getButtonArea(int btnId){
+	return ofRectangle(_buttons[btnId]._pos.x, 
+					   _buttons[btnId]._pos.y,
+					   _buttons[btnId]._img[_buttons[btnId]._id]->getWidth(),
+					   _buttons[btnId]._img[_buttons[btnId]._id]->getHeight());
+}
+
+void ofxButtons::changeButtonState(int btnId){
+	_buttons[btnId]._id  = (_buttons[btnId]._id + 1) % 2;
+}
+
+
+void ofxButtons::mousePressed(ofMouseEventArgs &e)
+{
+}
+void ofxButtons::mouseReleased(ofMouseEventArgs &e){
+	
+}
+
+void ofxButtons::addListener()
+{
+	ofAddListener(ofEvents().mousePressed, this, &ofxButtons::mousePressed);
+	ofAddListener(ofEvents().mouseReleased, this, &ofxButtons::mouseReleased);
+}
+void ofxButtons::removeListener()
+{
+	ofRemoveListener(ofEvents().mousePressed, this, &ofxButtons::mousePressed);
+	ofRemoveListener(ofEvents().mouseReleased, this, &ofxButtons::mouseReleased);
+}
+
+
+int ofxButtons::update(ofPixelsRef pixels){
+	float ratioX = (float)pixels.getWidth() / ofGetWidth();
+	float ratioY = (float)pixels.getHeight() / ofGetHeight();
+
+	map<int, button>::iterator it = _buttons.begin();
+	map<int, button>::iterator itend = _buttons.end();
+	for (; it != itend; ++it){
+		ofRectangle rect = getButtonArea(it->first);
+
+		rect.x = rect.x*ratioX;
+		rect.y = rect.y*ratioY;
+		rect.scaleWidth(ratioX);
+		rect.scaleHeight(ratioY);
+		int counts = countNoneZeroPixelIn(rect, pixels);
+		if (counts > 50){
+			return (int)it->first;
+		}
+	}
+
+	return -1;
+}
+
+int ofxButtons::countNoneZeroPixelIn(ofRectangle &rect, ofPixelsRef pixels)
+{
+	int count = 0;
+	for (int x = rect.x; x < rect.x + rect.width; ++x){
+		for (int y = rect.y; y < rect.y + rect.height; ++y){
+			if (x < pixels.getWidth() && y < pixels.getHeight())
+			if (pixels.getColor(x, y).r > 200){
+				count++;
+			}
+		}
+	}
+	return count;
+}
+
+// namespace }
+}
+
---
+++ ofxButtons.h
@@ -0,0 +1,95 @@
+#pragma once
+
+#include "ofxSimpleGuiToo.h"
+#include "ofMain.h"
+
+namespace cn_seefeel{
+
+enum myButtons{
+	BUTTON1 = 0,
+	BUTTON2,
+	BUTTON3,
+	BUTTON4,
+	BUTTON5,
+	BUTTON6,
+	BUTTON7,
+	BUTTON8,
+	BUTTON9,
+	BUTTON10,
+	BUTTON11,
+	BUTTON12,
+	BUTTON13,
+	BUTTON14,
+	BUTTON15
+};
+
+class button{
+
+public:
+
+	button():_pos(ofPoint(0, 0, 0)), 
+		_enabled(true), _id(0){
+			_img[0] = NULL;
+			_img[1] = NULL; 
+
+	}
+
+	ofImage		*_img[2];	// two state
+	
+	ofPoint		_pos;
+	bool		_enabled;
+
+	int			_id;
+	string		_title;
+};
+
+class ofxButtons
+{
+public:
+	
+	ofxButtons(void);
+	virtual ~ofxButtons(void);
+
+	void mousePressed(ofMouseEventArgs &e);
+	void mouseReleased(ofMouseEventArgs &e);
+
+	void addListener();
+	void removeListener();
+
+	/*
+	* set position configure page
+	*/
+	void setup(const string & name);
+
+	// used for button pressed check
+	/*
+	-1 : no button pressed
+	 0 : button1  pressed
+	 1 : button2  pressed
+	 ... ... 
+	*/
+	int update(ofPixelsRef pixels);
+
+	// used for openNI hand
+	void setPosition(const int& btnName, const ofPoint &pos);
+
+	void draw();
+	void draw(int btnId);
+
+	ofRectangle getButtonArea(int btnId);
+	void changeButtonState(int btnId);
+
+	void SetButton(const int &name, ofImage *b, int id = 0);
+	map<int,button> &buttons(){return _buttons;}
+
+protected:
+	int countNoneZeroPixelIn(ofRectangle &rect, ofPixelsRef pixels);
+private:
+	map<int, button> _buttons;
+
+	bool _bCentered;
+};
+
+}
+
+namespace cfeel = cn_seefeel;
