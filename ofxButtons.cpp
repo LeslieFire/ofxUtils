9e2979db69a65b5ac348341d970b96d89400b743
Yang Fei <249893977@qq.com>
2015-02-03 13:36:55 +0800

    fixed buttons

--- ofxButtons.cpp
+++ ofxButtons.cpp
@@ -1,35 +1,3 @@
-/*
- *  ofxButtons.cpp
- *
- *  Created by Leslie Yang on 2015-01-21
- *
- *
- *  Copyright 2015 https://github.com/LeslieFire All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the author nor the names of its contributors
- *       may be used to endorse or promote products derived from this software
- *       without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- *  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- *  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- *  OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *  ************************************************************************************ */ 
 #include "ofxButtons.h"
 namespace cn_seefeel{
 //--------------------------------class ofxButtons -----------------------
@@ -82,7 +50,7 @@ void ofxButtons::SetButton(const int &name, ofImage *b, int id){
 }
 
 void ofxButtons::draw(){
-
+	ofEnableAlphaBlending();
 	map<int, button>::iterator it = _buttons.begin();
 	map<int, button>::iterator itend = _buttons.end();
 
@@ -92,6 +60,7 @@ void ofxButtons::draw(){
 			it->second._img[it->second._id]->draw(it->second._pos.x, it->second._pos.y);
 		}
 	}
+	ofDisableAlphaBlending();
 }
 
 void ofxButtons::draw(int btnId){
@@ -172,6 +141,34 @@ int ofxButtons::countNoneZeroPixelIn(ofRectangle &rect, ofPixelsRef pixels)
 	return count;
 }
 
+int ofxButtons::update(ofPoint loc){
+	map<int, button>::iterator it = _buttons.begin();
+	map<int, button>::iterator itend = _buttons.end();
+	for (; it != itend; ++it){
+		if(isInRange(loc, getButtonArea(it->first))){
+			return (int)it->first;
+		}
+
+	}
+
+	return -1;
+}
+
+bool ofxButtons::isInRange(int x, int y, ofRectangle & t){
+	if ( ofInRange(x, t.getMinX(), t.getMaxX())
+		&& ofInRange(y, t.getMinY(), t.getMaxY())){
+			return true;
+	}
+	return false;
+}
+
+bool ofxButtons::isInRange(ofPoint loc, ofRectangle & t){
+	if ( ofInRange(loc.x, t.getMinX(), t.getMaxX())
+		&& ofInRange(loc.y, t.getMinY(), t.getMaxY())){
+			return true;
+	}
+	return false;
+}
 // namespace }
 }
 
--- ofxButtons.h
+++ ofxButtons.h
@@ -1,35 +1,3 @@
-/*
- *  G:\leslie\01github\ofxUtils\ofxButtons.h
- *
- *  Created by Leslie Yang on 2015-01-21
- *
- *
- *  Copyright 2015 https://github.com/LeslieFire All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the author nor the names of its contributors
- *       may be used to endorse or promote products derived from this software
- *       without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- *  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- *  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- *  OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *  ************************************************************************************ */ 
 #pragma once
 
 #include "ofxSimpleGuiToo.h"
@@ -52,7 +20,13 @@ enum myButtons{
 	BUTTON12,
 	BUTTON13,
 	BUTTON14,
-	BUTTON15
+	BUTTON15,
+	BUTTON16,
+	BUTTON17,
+	BUTTON18,
+	BUTTON19,
+	BUTTON20,
+	BUTTON21
 };
 
 class button{
@@ -91,7 +65,7 @@ public:
 	/*
 	* set position configure page
 	*/
-	void setup(const string & name);
+	virtual void setup(const string & name);
 
 	// used for button pressed check
 	/*
@@ -101,6 +75,7 @@ public:
 	 ... ... 
 	*/
 	int update(ofPixelsRef pixels);
+	int update(ofPoint loc);
 
 	// used for openNI hand
 	void setPosition(const int& btnName, const ofPoint &pos);
@@ -111,11 +86,13 @@ public:
 	ofRectangle getButtonArea(int btnId);
 	void changeButtonState(int btnId);
 
-	void SetButton(const int &name, ofImage *b, int id = 0);
+	virtual void SetButton(const int &name, ofImage *b, int id = 0);
 	map<int,button> &buttons(){return _buttons;}
 
 protected:
 	int countNoneZeroPixelIn(ofRectangle &rect, ofPixelsRef pixels);
+	bool isInRange(int x, int y, ofRectangle & t);
+	bool isInRange(ofPoint loc, ofRectangle & t);
 private:
 	map<int, button> _buttons;
 
